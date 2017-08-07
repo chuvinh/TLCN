cda7da80db2ea6b1627ea5921c96f80cbc8fa49e
vịnh lê văn <chuvinh.muadem@gmail.com>
2017-08-07 22:30:39 +0700

    LeVanVinh-7/8

---
+++ resources/views/donam.blade.php
@@ -0,0 +1,236 @@
+@extends('welcome')
+@section('title',' | Đồ nam')
+@section('content')
+<section class="menu_loc">
+	<div class="container-fluid">
+		<div></div>
+	</div>
+</section>
+<section class="content">
+	<div class="container-fluid">
+		<div class="col-md-12 hottrongngay">
+			<div class="content_title">
+				<hr>
+			</div>
+			<div class="col-md-8">
+				<div class="content_product">
+					<div class="product_slider_ao">
+	  					<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+					</div>
+					<div class="product_slider_quan">
+	  					<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+					</div>
+					<div class="product_slider_giay">
+	  					<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+						<div class="iteam_slider">
+	  						<div class="iteam">
+							    <img src="{{ asset('image/slider-05.jpg')}}" style="width: 100%">
+							    <button class="btn-default">Thêm vào giỏ</button>
+								<button class="btn-default">Mặc thử</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 box_thu">
+				<div class="col-md-12 box_ao">
+					<div class="iteam">
+						<p>Đây là hình áo 1</p>
+					</div>
+					<div class="iteam">
+						<p>Đây là hình áo 2</p>
+					</div>
+					<div class="iteam">
+						<p>Đây là hình áo 3</p>
+					</div>
+				</div>
+				<div class="col-md-12 box_quan">
+					<div class="iteam">
+						<p>Đây là hình quần 1</p>
+					</div>
+					<div class="iteam">
+						<p>Đây là hình quần 2</p>
+					</div>
+					<div class="iteam">
+						<p>Đây là hình quần 3</p>
+					</div>
+				</div>
+				<div class="col-md-12 box_giay">
+					<div class="iteam">
+						<p>Đây là hình giày 1</p>
+					</div>
+					<div class="iteam">
+						<p>Đây là hình giày 2</p>
+					</div>
+					<div class="iteam">
+						<p>Đây là hình giày 3</p>
+					</div>
+				</div>
+				<div class="btn_giaodich">
+					<button class="btn btn-default">Mua cả bộ</button>
+					<button class="btn btn-default">Hủy</button>
+				</div>
+			</div>
+		</div>
+		<div></div>
+	</div>
+</section>
+@stop
